760c9600a2ddb55d6a43705db546bb51599e0bee
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2023-11-19 13:29:40 +0300

    fixing syntax

--- packages/hardhat/test/YourContract.ts
+++ packages/hardhat/test/YourContract.ts
@@ -16,18 +16,19 @@ describe("YourContract", function () {
     it("Should have the right message on deploy", async function () {
       expect(await yourContract.greeting()).to.equal("Hello Builders!");
     });
+
     it("Should allow setting a new greeting", async function () {
       const newGreeting = "Learn Scaffold-ETH 2! :)";
-
       await yourContract.setGreeting(newGreeting);
       expect(await yourContract.greeting()).to.equal(newGreeting);
     });
+
     it("Should increment the counter after a new greeting", async function () {
       expect(await yourContract.totalCounter()).to.equal(1);
     });
+
     it("Should emit a GreetingChange event after a new greeting", async function () {
       const newGreeting = "Testing an event!";
-
       await expect(yourContract.setGreeting(newGreeting))
         .to.emit(yourContract, "GreetingChange")
         .withArgs("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", newGreeting);
